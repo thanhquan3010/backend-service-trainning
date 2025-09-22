0771a63ccf8a7a2e1357edda0b24ea5672e8b79a
thanhquan3010 <tranthanhquan09@gmail.com>
2025-09-22 11:20:33 +0700

    config profile in pom.xml, create environment resources files

--- backend-service/src/main/java/vn/thanhquan/BackendServiceApplication.java
+++ backend-service/src/main/java/vn/thanhquan/BackendServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServiceApplication.class, args);
 	}
