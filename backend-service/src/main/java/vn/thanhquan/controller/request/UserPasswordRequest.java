9fb27c08cd258b1ebaae4be3f5a0d994479ccecb
thanhquan3010 <tranthanhquan09@gmail.com>
2025-10-05 18:42:59 +0700

    validate for UpdatePasswordRequest

--- backend-service/src/main/java/vn/thanhquan/controller/request/UserPasswordRequest.java
+++ backend-service/src/main/java/vn/thanhquan/controller/request/UserPasswordRequest.java
@@ -1,10 +1,17 @@
 package vn.thanhquan.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserPasswordRequest {
     private Long id;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    @Size(min = 6, message = "Mật khẩu phải có ít nhất 6 ký tự")
     private String password;
+
+    @NotBlank(message = "Xác nhận mật khẩu không được để trống")
     private String confirmPassword;
 }
